6f1e91473e11a897a451f1fecb55c073feeb76fe
jayantgaikwad <jayantgaikwad8@gmail.com>
2022-10-04 08:46:32 +0530

    add basic template

---
+++ src/Component/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+export default function Header() {
+    return (
+            <nav>
+            <img src="markup\public\react_logo.png"
+            alt="React-logo"></img>
+            <h3>ReactFacts</h3>
+            <h4>React Course - Project 1</h4>
+            </nav>
+    )
+}
---
+++ src/Component/Main.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default function main() {
+
+    return (
+        <h1>Hello main componet</h1>
+    )
+}
